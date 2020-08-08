c4654af120ac87a7ce3c4953a4e4b362f2c1c7a9
Sashvat Srivastava <67827299+sashvatSrivastava@users.noreply.github.com>
2020-08-08 19:56:08 +0530

    Add files via upload

---
+++ class)(object.js
@@ -0,0 +1,35 @@
+// class below this line
+class person {
+	constructor(firstName, lastName, name){ // this line contains the constructor
+	this.firstName = firstName ;
+	this.lastName = lastName ;
+	this.name = name ;
+ }  
+}
+
+// object below this line
+var firstName = "Sashvat" 
+var lastName  = "Srivastava" ; 
+
+var name = firstName + lastName  ;
+
+console.log(name);
+
+/* class- A class is a type of function,
+ but instead of using the keyword function to initiate it,
+  we use the keyword class,
+   and the properties are assigned inside a
+    constructor() method. */
+
+    /* object- In JavaScript, almost "every value" is an object.
+     every value except for 
+1)number
+2)boolean
+3)null
+4)undefined
+5)string
+
+is an object*/
+
+// information taken from  https://www.w3schools.com/js
+
